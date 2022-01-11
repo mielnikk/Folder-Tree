05434d8e76ca9ac6f70e82bc75e2a7c8fe125dc1
Katarzyna Mielnik <kasia.a.mielnik@gmail.com>
2022-01-11 16:32:49 +0100

    node przeniesione do osobnego pliku

--- Tree.c
+++ Tree.c
@@ -7,38 +7,12 @@
 #include "HashMap.h"
 #include "path_utils.h"
 #include "err.h"
+#include "Node.h"
 
-#define WRITE_ACCESS -1
-
-typedef struct Node Node;
-
-struct Node {
-    HashMap *children;
-
-    pthread_mutex_t mutex;
-    pthread_cond_t read_cond; /* condition for readers to wait on */
-    pthread_cond_t write_cond; /* condition for writers to wait on */
-    int readers_count;
-    int writers_count;
-
-    int writers_waiting;
-    int readers_waiting;
-
-    /* Helps to indicate whether a woken process was actually signaled.
-     * If set to WRITE_ACCESS, indicates that a signaled writer may access the critical section.
-     * If set to 0, any process can access the critical section if there are no other processes that
-     * forbid it.
-     * If the value is >0, a signaled reader may access the critical section. */
-    int change;
-
-    /* a process waiting on this condition is going to be the last process to access the node */
-    pthread_cond_t move_cond;
-};
 
 struct Tree {
     Node *root;
 };
-
 
 void delete_node(Node *node) {
     if (pthread_mutex_destroy(&node->mutex) != 0)
@@ -73,132 +47,6 @@ Node *new_node() {
     node->children = hmap_new();
 
     return node;
-}
-
-/* Acquires read access to `node`. */
-void get_read_access(Node *node) {
-    if (!node)
-        return;
-
-    if (pthread_mutex_lock(&node->mutex) != 0)
-        syserr("lock failed");
-
-    node->readers_waiting++;
-    assert(node->readers_waiting >= 0);
-    while (node->writers_count + node->writers_waiting > 0 && node->change <= 0) {
-        if (pthread_cond_wait(&node->read_cond, &node->mutex) != 0)
-            syserr("read cond wait failed");
-    }
-    node->readers_waiting--;
-    assert(node->readers_waiting >= 0);
-    if (node->change > 0)
-        node->change--;
-
-    node->readers_count++;
-
-    assert(node->writers_count == 0);
-    assert(node->change >= 0);
-    assert(node->readers_count > 0);
-
-    if (node->change > 0)
-        if (pthread_cond_signal(&node->read_cond) != 0)
-            syserr("read cond signal failed");
-
-    if (pthread_mutex_unlock(&node->mutex) != 0)
-        syserr("unlock failed");
-}
-
-/* Releases read access to `node`. */
-void give_up_read_access(Node *node) {
-    if (pthread_mutex_lock(&node->mutex) != 0)
-        syserr("mutex lock failed");
-
-    node->readers_count--;
-    assert(node->readers_count >= 0);
-    assert(node->writers_count == 0);
-    if (node->readers_count == 0 && node->writers_waiting > 0) {
-        node->change = WRITE_ACCESS;
-        if (pthread_cond_signal(&node->write_cond) != 0)
-            syserr("modify cond wait failed");
-    }
-    else if (node->readers_count == 0 && node->writers_waiting == 0) {
-        if (pthread_cond_signal(&node->move_cond) != 0)
-            syserr("move cond wait failed");
-    }
-
-    if (pthread_mutex_unlock(&node->mutex) != 0)
-        syserr("unlock failed");
-}
-
-/* Acquires write access to `node` */
-void get_write_access(Node *node) {
-    if (!node)
-        return;
-
-    if (pthread_mutex_lock(&node->mutex) != 0)
-        syserr("lock failed");
-
-
-    node->writers_waiting++;
-    while (node->writers_count + node->readers_count > 0 && node->change != WRITE_ACCESS) {
-        if (pthread_cond_wait(&node->write_cond, &node->mutex) != 0)
-            syserr("modify cond wait failed");
-    }
-    node->writers_waiting--;
-
-    assert(node->readers_count == 0);
-    assert(node->writers_count == 0);
-
-    node->change = 0;
-    node->writers_count++;
-
-    if (pthread_mutex_unlock(&node->mutex) != 0)
-        syserr("unlock failed");
-}
-
-/* Releases write access to `node`. */
-void give_up_write_access(Node *node) {
-    if (pthread_mutex_lock(&node->mutex) != 0)
-        syserr("lock failed");
-
-    node->writers_count--;
-    assert(node->writers_count == 0);
-    if (node->readers_waiting > 0) {
-        node->change = node->readers_waiting;
-        if (pthread_cond_signal(&node->read_cond) != 0)
-            syserr("read cond signal failed");
-    }
-    else if (node->writers_waiting > 0) {
-        node->change = WRITE_ACCESS;
-        if (pthread_cond_signal(&node->write_cond) != 0)
-            syserr("write cond signal failed");
-    }
-    else {
-        if (pthread_cond_signal(&node->move_cond) != 0)
-            syserr("move cond wait failed");
-    }
-
-    if (pthread_mutex_unlock(&node->mutex) != 0)
-        syserr("unlock failed");
-}
-
-/* Acquires write access to `node`. If any other processes are waiting or working,
- * lets them access `node` first.
- * To avoid starvation, a write lock on `node`'s parent is needed, as it blocks
- * new incoming processes. */
-void get_move_access(Node *node) {
-    if (pthread_mutex_lock(&node->mutex) != 0)
-        syserr("lock failed");
-
-    while (node->writers_waiting + node->writers_count
-           + node->readers_waiting + node->readers_count > 0) {
-        if (pthread_cond_wait(&node->move_cond, &node->mutex) != 0)
-            syserr("modify cond wait failed");
-    }
-    node->change = 0;
-
-    if (pthread_mutex_unlock(&node->mutex) != 0)
-        syserr("unlock failed");
 }
 
 /* Acquires write access to the folder indicated by the path.
