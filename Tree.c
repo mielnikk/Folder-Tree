ee868146ec6738909d2df80a2dd841ab365edbb3
Katarzyna Mielnik <kasia.a.mielnik@gmail.com>
2022-01-13 23:06:51 +0100

    usunięty zbędny warunek i memleak

--- Tree.c
+++ Tree.c
@@ -325,7 +325,7 @@ int tree_move(Tree *tree, const char *source, const char *target) {
     char source_name[MAX_FOLDER_NAME_LENGTH + 1];
     char *initial_subpath_source = make_path_to_parent(source + common_length - 1, source_name);
     const char *source_subpath = initial_subpath_source;
-    
+
     Node *source_parent = write_folder(lca, source_subpath, true);
     free(initial_subpath_source);
 
